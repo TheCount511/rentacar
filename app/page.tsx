6edb9515d1e5598c205249bb4288d79d309c2740
TheCount511 <fadhil.akins@gmail.com>
2023-06-20 03:45:05 -0600

    minor changes and bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -37,8 +37,8 @@ export default async function Home({
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars.map((car) => (
-                <CarCard car={car} key={car.model} />
+              {allCars.map((car, index) => (
+                <CarCard car={car} key={`${car.model}${index}}`} />
               ))}
             </div>
             <ShowMore
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" priority />
         </div>
         <div className="hero__image-overlay"></div>
       </div>
