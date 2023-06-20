61f983c4260d7c95a6e7e6b21c7d946b6fcaa6cf
TheCount511 <fadhil.akins@gmail.com>
2023-06-20 02:45:28 -0600

    initial commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+// experimental: {
+//   appDir: true,
+// },
+// pakcage json next version //     "next": "13.4.5",
+
+module.exports = nextConfig;
---
+++ types/index.ts
@@ -0,0 +1,70 @@
+import { Dispatch, MouseEventHandler, SetStateAction } from "react";
+
+export interface CustomButtonProps {
+  title: string;
+  containerStyles?: string;
+  textStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  rightIcon?: string;
+  isDisabled?: boolean;
+}
+
+export interface SearchBarProps {
+  setManufacturer: (searchManufacturer: string) => void;
+  setModel: (searchModel: string) => void;
+}
+// export interface SearchManufacturerProps {
+//   selected: string;
+//   setSelected: (selected: string) => void;
+// }
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+type StringOrNumber = string | number;
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+  setFilter?: Dispatch<SetStateAction<string> | SetStateAction<number>>;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface CarCardProps {
+  car: CarProps;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+  message?: string;
+}
+
+export interface showMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+  setLimit?: Dispatch<SetStateAction<number>>;
+}
